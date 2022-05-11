67b11e7d16beb928e5054e5aecace1d78b7a8225
Alexandre FLAMENT <alexandre.flament@hesge.ch>
2022-05-11 14:59:53 +0000

    Update FastAPI version

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 import logging
+import asyncio
+import multiprocessing
 from time import time
+from itertools import chain, islice
 from typing import Dict, List, Optional, Union
 
+import orjson
 import aiohttp
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Body, Query
 from fastapi.middleware.gzip import GZipMiddleware
-from fastapi.responses import JSONResponse
+from fastapi.responses import ORJSONResponse
 from pydantic import BaseModel
+from logging import Logger
 
 import diskmap
 import matchingalgorithm
 import server
+import utils
+
+
+logger = logging.getLogger(__name__)
 
 MATCHING_DB = diskmap.DiskMap("matching.lmdb")
 HTTP_SESSION: aiohttp.ClientSession = None
 CONFIG = server.read_config("""
 [server]
-# see https://pgjones.gitlab.io/hypercorn/reference/source/hypercorn.config.html#hypercorn.config.Config
 root_path=/
-bind=localhost:8888
+host=localhost
+port=8888
+default_proc_name=ebiodiv
+# ssl_keyfile=
+# ssl_certfile=
 
 [datasource]
 url=https://tb.plazi.org/GgServer/gbifOccLinkData/
@@ -26,13 +38,13 @@ timeout=180
 """)
 DATASOURCE = CONFIG["datasource"]
 
-logger = logging.getLogger("ebiodiv")
-logger_datasource = logger.getChild("datasource")
 app = FastAPI(
     title="eBioDiv - Backend API",
     version="2.0.0",
     docs_url="/",
     redoc_url=None,
+    root_path=CONFIG["server"]["root_path"],
+    default_response_class=ORJSONResponse,
     swagger_ui_parameters={"syntaxHighlight": False},
     description="""
 <p>See the <a href="https://candy.text-analytics.ch/eBioDiv/">project page</a></p>
@@ -46,12 +58,15 @@ but add scoring between the occurrences</p>
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 
+server.configure_app(app)
+
+
 async def catch_exceptions_middleware(request: Request, call_next):
     try:
         return await call_next(request)
     except Exception as exc:
         logger.exception("Exception")
-        return JSONResponse(
+        return ORJSONResponse(
             status_code=500,
             content={
                 "error": exc.__class__.__module__ + "." + exc.__class__.__name__,
@@ -65,11 +80,12 @@ app.middleware("http")(catch_exceptions_middleware)
 
 
 async def on_request_end(session, trace_config_ctx, params):
-    logger_datasource.info(f"\"{params.method} {params.url}\" {params.response.status} {params.response.headers.get('content-length', '')}")
+    logger.info(f"\"{params.method} {params.url}\" {params.response.status} {params.response.headers.get('content-length', '')}")
 
 
 @app.on_event("startup")
 async def startup_event():
+    """create HTTP client & log outgoing HTTP request"""
     global HTTP_SESSION
     trace_config = aiohttp.TraceConfig()
     trace_config.on_request_end.append(on_request_end)
@@ -120,6 +136,16 @@ def get_relation_id(occurrenceKey1, occurrenceKey2):
     return str(relation_keys[0]) + "," + str(relation_keys[1])
 
 
+def _add_score_on_chunk(normalized_occ_dict, chunk: List[Dict]) -> List[Dict]:
+    # get the scores from the normalized occurrences
+    # leave the original occurrences untouched
+    for relation in chunk:
+        o1 = normalized_occ_dict[relation["occurrenceKey1"]]
+        o2 = normalized_occ_dict[relation["occurrenceKey2"]]
+        relation["scores"] = matchingalgorithm.get_scores(o1, o2)
+    return chunk
+
+
 def _add_score(data) -> None:
     # normalized a copy of the occurrences
     normalized_occ_dict = {}
@@ -127,16 +153,26 @@ def _add_score(data) -> None:
         normalized_occ = occ.copy()
         matchingalgorithm.normalize_occurrence(normalized_occ)
         normalized_occ_dict[int(occ_key)] = normalized_occ
-    # get the scores from the normalized occurrences
-    # leave the original occurrences untouched
-    for relation in data["occurrenceRelations"]:
-        o1 = normalized_occ_dict[relation["occurrenceKey1"]]
-        o2 = normalized_occ_dict[relation["occurrenceKey2"]]
-        relation["scores"] = matchingalgorithm.get_scores(o1, o2)
+
+    # few relations: sync call
+    if len(data["occurrenceRelations"]) < 200:
+        _add_score_on_chunk(normalized_occ_dict, data["occurrenceRelations"])
+        return
+
+    # a lot of relations: use a process pool (~1.6 seconds for 5000 relations)
+    chunk_size = max(100, int(len(data["occurrenceRelations"]) / utils.get_worker_count()) + 1)
+    chunks = utils.chunked(data["occurrenceRelations"], chunk_size)
+    data["occurrenceRelations"] = utils.pool_map(_add_score_on_chunk, chunks, normalized_occ_dict)
 
 
 @app.get("/occurrences", description="list of occurrences", tags=["data"])
-async def get_occurrences(institutionKey: Optional[str] = None, datasetKey: Optional[str] = None, occurrenceKeys: Optional[str] = None, scores: bool = False):
+async def get_occurrences(
+    institutionKey: Optional[str] = None,
+    datasetKey: Optional[str] = None,
+    occurrenceKeys: Optional[str] = None,
+    fetchMissing: Optional[bool] = Query(default=None, description="Fetch missing occurrences, allow to add new occurrences"),
+    scores: bool = False
+):
     params = {}
     if institutionKey is not None:
         params["institutionKey"] = institutionKey
@@ -144,11 +180,16 @@ async def get_occurrences(institutionKey: Optional[str] = None, datasetKey: Opti
         params["datasetKey"] = datasetKey
     if occurrenceKeys is not None:
         params["occurrenceKeys"] = occurrenceKeys
+    if fetchMissing is not None:
+        params["fetchMissing"] = "true" if fetchMissing else "false"
     async with HTTP_SESSION.get(DATASOURCE["url"] + "occurrences", params=params) as response:
         if response.status != 200:
             # error: proxy the response
             return Response(await response.read(), status_code=response.status, media_type=response.headers["Content-Type"])
-        data = await response.json()
+
+        content = await response.read()
+        # orjson.loads(content) takes a few seconds on a large documents (>10MB).
+        data = orjson.loads(content)
 
         # add matching
         for relation in data["occurrenceRelations"]:
@@ -157,8 +198,9 @@ async def get_occurrences(institutionKey: Optional[str] = None, datasetKey: Opti
 
         # add scores
         if scores:
-            _add_score(data)
-        return data
+            await asyncio.get_event_loop().run_in_executor(None, _add_score, data)
+
+        return ORJSONResponse(data)
 
 
 class OccurrenceMatching(BaseModel):
@@ -168,7 +210,7 @@ class OccurrenceMatching(BaseModel):
     comment: Optional[str] = None
 
 
-@app.post("/matching", description='Update the "match" value between two occurrences', tags=["matching"])
+@app.post("/matching", description='Update the "match" value between two occurrences. ⚠️ DEPRECATED use /occurrenceRelations', tags=["matching"])
 async def add_matching(matchingInput: List[OccurrenceMatching]):
     now = int(time())
     response = []
@@ -180,5 +222,11 @@ async def add_matching(matchingInput: List[OccurrenceMatching]):
     return response
 
 
+@app.post("/occurrenceRelations", description='Update the "match" value between occurrences', tags=["matching"])
+async def occurrence_relations(data = Body(default=None, example="""{"body":{"occurrenceRelations":[{"occurrenceKey1":20,"occurrenceKey2":42,"decision":null},{"occurrenceKey1":20,"occurrenceKey2":42,"decision":true},{"occurrenceKey1":20,"occurrenceKey2":42,"decision":false}]}}""")):
+    async with HTTP_SESSION.post(DATASOURCE["url"] + "occurrenceRelations", json=data) as response:
+        return Response(await response.read(), status_code=response.status, media_type=response.headers["Content-Type"])
+
+
 if __name__ == "__main__":
-    server.run(CONFIG["server"], __name__ + ":app")
+    server.run(CONFIG["server"], "app:app")
--- server.py
+++ server.py
@@ -1,54 +1,209 @@
+import os
+import sys
 import configparser
 import logging
 import multiprocessing
-import sys
+import argparse
+import cProfile
+from os import path
+from contextlib import contextmanager
 
 import coloredlogs
-import hypercorn.run
-from hypercorn.config import Config
+import uvicorn
+import uvicorn.config
+from uvicorn.importer import import_from_string
+import gunicorn.app.base
+from gunicorn.glogging import Logger
 
-production = True if len(sys.argv) > 1 and sys.argv[1] == "prod" else False
-coloredlogs.install(
-    level="WARNING" if production else "DEBUG",
-    fmt="%(asctime)s [%(process)d] %(name)-30.30s %(levelname)-7s %(message)s",
-    field_styles=dict(
-        asctime=dict(color="blue"),
-        process=dict(color="blue"),
-        name=dict(color="cyan"),
-        hostname=dict(color="magenta"),
-        programname=dict(color="cyan"),
-        levelname=dict(color="black", bold=True),
-        username=dict(color="yellow"),
-    ),
-)
-logging.getLogger("asyncio").setLevel(logging.ERROR)
+
+# Debug
+LOG_FORMAT_DEBUG = '%(levelname)-7s %(name)-30.30s: %(message)s'
+
+# Production
+LOG_FORMAT_PROD = '%(asctime)-15s | %(process)d | %(levelname)s | %(name)s | %(message)s'
 
 
 def read_config(default_config) -> configparser.ConfigParser:
     config = configparser.ConfigParser()
     config.read_string(default_config)
-    config.read('server.ini')
+    config.read("server.ini")
     return config
 
 
-def run(config_mapping, application_path):
-    config = Config.from_mapping(config_mapping)
-    config.application_path = application_path
-    if production:
-        if "worker" not in config_mapping:
-            config.workers = min(8, multiprocessing.cpu_count())
-        if config.accesslog is None:
-            config.accesslog = config.errorlog
+## LOGGING
+
+def configure_logging():
+    global args
+    log_level = "INFO" if args.production else "DEBUG"
+    log_format = LOG_FORMAT_PROD if args.production else LOG_FORMAT_DEBUG
+
+    logging.getLogger("asyncio").setLevel(logging.ERROR)
+
+    level_styles = {
+        'spam': {'color': 'green', 'faint': True},
+        'debug': {},
+        'notice': {'color': 'magenta'},
+        'success': {'bold': True, 'color': 'green'},
+        'info': {'bold': True, 'color': 'cyan'},
+        'warning': {'color': 'yellow'},
+        'error': {'color': 'red'},
+        'critical': {'bold': True, 'color': 'red'},
+    }
+    field_styles = {
+        'asctime': {'color': 'green'},
+        'process': {'color': 'green'},
+        'hostname': {'color': 'magenta'},
+        'levelname': {'color': 8},
+        'name': {'color': 8},
+        'programname': {'color': 'cyan'},
+        'username': {'color': 'yellow'},
+    }
+    coloredlogs.install(level=log_level, level_styles=level_styles, field_styles=field_styles, fmt=log_format, reconfigure=True, milliseconds=True)
+
+
+def configure_app(app):
+    app.on_event("startup")(configure_logging)
+
+
+## RUN PRODUCTION
+
+class StubbedGunicornLogger(Logger):
+    def setup(self, cfg):
+        configure_logging()
+        self.error_logger = logging.getLogger("gunicorn.error")
+        self.access_logger = logging.getLogger("gunicorn.access")
+        self.error_logger.setLevel("INFO")
+        self.access_logger.setLevel("INFO")
+        self.access_logger.addHandler(logging.root.handlers[0])
+        self.error_logger.addHandler(logging.root.handlers[0])
+        self.error_logger.propagate = False
+        self.access_logger.propagate = False
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    def __init__(self, app_name, options=None):
+        self.options = options or {}
+        self.application = import_from_string(app_name)
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
+def run_production(config, app_name, args):
+    if "worker" not in config:
+        workers = min(8, multiprocessing.cpu_count())
     else:
-        config.accesslog = config.errorlog = logging.getLogger("server")
-        config.debug = True
-        config.use_reloader = True
-        config.access_log_format = '%(h)s "%(r)s" %(s)s %(l)s %(b)s bytes in %(L)s sec'
+        workers = int(config.get("worker", "1"))
+    options = {
+        "bind": "%s:%s" % (config["host"], config["port"]),
+        "workers": workers,
+        "accesslog": "-",
+        "errorlog": "-",
+        "logger_class": StubbedGunicornLogger,
+        "worker_class": "uvicorn.workers.UvicornWorker",
+        "default_proc_name": config.get("default_proc_name", "gunicorn"),
+        "keyfile": config.get("ssl_keyfile"),
+        "certfile": config.get("ssl_certfile"),
+    }
+    StandaloneApplication(app_name, options).run()
+
+
+## RUN DEBUG
+
+@contextmanager
+def profile_context(profile_filename):
+    logger = logging.getLogger(__name__)
+    logger.warning('Start profiling, the reload option is disabled')
+    pr = cProfile.Profile()
+    pr.enable()
     try:
-        import uvloop
+        yield
+    finally:
+        pr.disable()
+        pr.dump_stats(profile_filename)
+        logger.info('Profiling recorded on %s', profile_filename)
 
-        config.worker_class = "uvloop"
-    except ImportError:
-        pass
 
-    hypercorn.run.run(config)
+def get_reload_excludes():
+    if path.exists('.gitignore'):
+        with open('.gitignore') as f:
+            for file_name in f.readlines():
+                yield file_name.replace('\n', '')
+
+
+def run_debug(config, app_name, args):
+    # normal configuration
+    uvicorn_logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "()": "uvicorn.logging.DefaultFormatter",
+                "fmt": "%(message)s",
+                "use_colors": None,
+            },
+            "access": {
+                "()": "uvicorn.logging.AccessFormatter",
+                "fmt": '%(client_addr)s - "%(request_line)s" %(status_code)s',  # noqa: E501
+            },
+        },
+        "loggers": {
+            "uvicorn": {"level": "INFO", "propagate": True},
+            "uvicorn.error": {"level": "ERROR", "propagate": True},
+            "uvicorn.access": {"level": "INFO", "propagate": True},
+        },
+    }
+
+    uvicorn_kwargs = dict(
+        host=config["host"],
+        port=int(config["port"]),
+        log_level="debug",
+        log_config=uvicorn_logging_config,
+        access_log=True,
+        ssl_keyfile= config.get("ssl_keyfile"),
+        ssl_certfile= config.get("ssl_certfile"),
+    )
+
+    if not args.profile_filename:
+        uvicorn.run(
+            app_name,
+            reload=True,
+            reload_excludes=list(get_reload_excludes()),
+            **uvicorn_kwargs
+        )
+    else:
+        with profile_context(args.profile_filename):
+            uvicorn.run(
+                import_from_string(app_name),
+                **uvicorn_kwargs
+            )
+
+## RUN
+
+def parse_args(app_name = ""):
+    parser = argparse.ArgumentParser(description=app_name)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--production', dest='production', action='store_true',
+                       help='Run in production mode')
+    group.add_argument('--profile', type=str, dest='profile_filename',
+                       help='Run cProfile in developpment mode and record the a .prof file',
+                       default=None)
+    return parser.parse_args()
+
+
+def run(config, app_name):
+    args = parse_args(app_name)
+    configure_logging()
+    if args.production:
+        run_production(config, app_name, args)
+    else:
+        run_debug(config, app_name, args)
+
+
+args = parse_args()
