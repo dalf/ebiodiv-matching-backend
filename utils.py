0a6c0b91f0dbf19bc396a976fceb67d8418ee189
Alexandre FLAMENT <alexandre.flament@hesge.ch>
2022-05-11 15:34:27 +0000

    Add missing utils.py

---
+++ utils.py
@@ -0,0 +1,73 @@
+import multiprocessing
+from itertools import chain, islice
+from multiprocessing import Pool
+from typing import List, Iterator, TypeVar, Generator, Tuple
+from collections.abc import Callable
+
+
+__all__ = ['chunked', 'pool_map', 'get_worker_count']
+
+
+def get_worker_count():
+    return multiprocessing.cpu_count()
+
+
+T = TypeVar('T')
+
+
+def chunked(seq: Iterator[T], chunksize: int) -> Generator[List[T], None, None]:
+    """Yields items from an iterator in iterable chunks."""
+    it = iter(seq)
+    while True:
+        try:
+            yield list(chain([next(it)], islice(it, chunksize-1)))
+        except StopIteration:
+            break
+
+
+# def _worker_wrapper(t: Tuple[Callable[..., List[T]], List[T]]) -> List[T]:
+def _worker_wrapper(t):
+    f, chunk = t
+    g = globals()
+    return f(*g['__args'], chunk, **g['__kwargs'])
+
+
+# def pool_map(f: Callable[..., List[T]], chunk_list: Iterator[List[T]], *args, **kwargs) -> List[T]:
+def pool_map(f, chunk_list, *args, **kwargs):
+    """Equivalent of multiprocessing.Pool.map() but allows additional arguments (*args and **kwargs).
+    The additional parameters are serialized once per worker.
+
+    Equivalent of the following code, but each call to f happens in a different processes to overcome the GIL:
+
+    >>> result = []
+    >>> for chunk in chunk_list:
+    >>>     result.extend(f(*args, chunk, **kwargs))
+    >>> return result
+
+    Also equivalent of (but this pool_map is faster when *args and **kwargs takes a lot of memory):
+
+    >>> import functools
+    >>> result = []
+    >>> with Pool(get_worker_count()) as p:
+    >>>     fp = functool.partial(f, *args, **kwargs)
+    >>>     for chunk in p.map(fp, chunk_list):
+    >>>         result.extend(chunk)
+    >>> return result
+
+    """
+    def init_worker():
+        globals()['__args'] = args
+        globals()['__kwargs'] = kwargs
+
+    results = []
+
+    worker_data: List[Tuple[Callable[..., List[T]], List[T]]] = [(f, chunk) for chunk in chunk_list]
+
+    with Pool(
+        processes=min(get_worker_count(), len(worker_data)),
+        initializer=init_worker,
+    ) as p:
+        for chunk in p.map(_worker_wrapper, worker_data):
+            results.extend(chunk)
+
+    return results
